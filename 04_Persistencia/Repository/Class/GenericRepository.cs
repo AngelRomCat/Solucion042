ab1fb0d2d12b325c7495298a759e9ec529ee7f6a
Àngel <angel@rom.cat>
2022-10-25 19:39:09 +0200

    Archivos GenericRepository y UnitOfWork Creados

---
+++ 04_Persistencia/Repository/Class/GenericRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using _05_Data;
+using _05_Data.Data;
+
+namespace _04_Persistencia.Repository.Class
+{
+    public class GenericRepository<T> where T : class
+    {
+        public NorthWindTuneadoDbContext Db { get; set; }
+
+        public GenericRepository(NorthWindTuneadoDbContext context)
+        {
+            Db = context;
+        }
+
+        public virtual IList<T> GetAll()
+        {
+            IQueryable<T> query = Db.Set<T>();
+            return query.ToList();
+        }
+
+        public IList<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            var query = Db.Set<T>().Where(predicate);
+            return query.ToList();
+        }
+
+        public virtual bool Add(T entity)
+        {
+            Db.Set<T>().Add(entity);
+            return true;
+        }
+
+        public virtual bool Exists(T entity)
+        {
+            return Db.Set<T>().Any();
+
+        }
+        public virtual T AddAndGet(T entity)
+        {
+            Db.Set<T>().Add(entity);
+            return entity;
+        }
+        public virtual bool Delete(T entity)
+        {
+            Db.Entry(entity).State = EntityState.Modified;
+            Db.Set<T>().Remove(entity);
+            return true;
+        }
+
+        public virtual bool Edit(T entity)
+        {
+            Db.Entry(entity).State = EntityState.Modified;
+            return true;
+        }
+        public virtual bool Save()
+        {
+            Db.SaveChanges();
+            return true;
+        }
+
+        public virtual bool SaveChanges(T entity)
+        {
+            if (Db.Entry(entity).State == EntityState.Detached)
+            {
+                Db.Set<T>().Attach(entity);
+            }
+            Save();
+            return true;
+        }
+
+        public virtual T FindById(int id)
+        {
+            return Db.Set<T>().Find(id);
+        }
+
+        public virtual void Attach(T entity)
+        {
+            Db.Set<T>().Attach(entity);
+        }
+    }
+
+}
---
+++ 04_Persistencia/UoW/Class/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using _04_Persistencia.Repository.Class;
+using _05_Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Persistencia.UoW.Class
+{
+    public class UnitOfWork
+    {
+        private readonly NorthWindTuneadoDbContext _context;
+
+        private GenericRepository<Cliente> ClienteRepository;
+
+        private bool _disposed;
+
+        public UnitOfWork()
+        {
+            _context = new NorthWindTuneadoDbContext();
+        }
+
+        //Repositories
+
+        public GenericRepository<Cliente> ClienteRepository => clienteRepository ??
+                                                             (clienteRepository = new GenericRepository<Cliente>(_context));
+
+        ////Esto también se puede poner así en forma de PROPERTY que da valor al 
+        ////private Atributo clienteRepository 
+        ////mediante un Getter:
+        //GenericRepository<Cliente> ClienteRepository
+        //{
+        //    get
+        //    {
+        //        return clienteRepository ?? (clienteRepository = new GenericRepository<Cliente>(_context));
+        //    }
+        //}
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+                if (disposing) _context.Dispose();
+            _disposed = true;
+        }
+    }
+
+}
